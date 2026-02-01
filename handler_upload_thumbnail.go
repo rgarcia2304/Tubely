930d60d230b08406de33074fdd1ca672e0a1056f
rgarcia2304 <garcia2304@icloud.com>
2026-02-01 08:08:46 -0500

    batman

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -6,9 +6,11 @@ import (
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
 	"github.com/google/uuid"
+	"io"
 )
 
 func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Request) {
+	// validating the request
 	videoIDString := r.PathValue("videoID")
 	videoID, err := uuid.Parse(videoIDString)
 	if err != nil {
@@ -32,6 +34,48 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
 	// TODO: implement the upload here
+	const maxMemory =  10 << 20
+	r.ParseMultipartForm(maxMemory)
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	// "thumbnail" should match the HTML form input name
+	file, header, err := r.FormFile("thumbnail")
+	if err != nil{
+		respondWithError(w, http.StatusBadRequest, "Unable to parse form file", err)
+		return
+	}
+
+	defer file.Close()
+
+	imageData, err := io.ReadAll(file)
+	if err != nil{
+		respondWithError(w, http.StatusBadRequest, "Unable to read image data", err)
+		return
+	}
+	
+	// get the videos metadata
+	vidData, err := cfg.db.GetVideo(videoID)
+	if err != nil{
+		respondWithError(w, http.StatusBadRequest, "Unable to fetch the video with provided ID", err)
+		return 
+	}
+
+	if vidData.CreateVideoParams.UserID != userID{
+		respondWithJSON(w, http.StatusUnauthorized, struct{}{})
+		return 
+	}
+
+	//create the new thumbnail struct 
+	newThumbnail := thumbnail{data: imageData, mediaType: header.Header.Get("Content-Type")}
+	videoThumbnails[vidData.ID] = newThumbnail
+
+	url := "http://localhost:8091/api/thumbnails/" + videoIDString 
+	vidData.ThumbnailURL = &url
+	err = cfg.db.UpdateVideo(vidData)
+	
+	if err != nil{
+		respondWithError(w, http.StatusBadRequest, "Unable to update the video", err)
+		return
+	}
+	
+	respondWithJSON(w, http.StatusOK, newThumbnail)
 }
